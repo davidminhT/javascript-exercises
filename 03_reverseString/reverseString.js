4767875f1efa095dfd5ce28073bf6e67df9eaad6
David Truong <davidtruong063@gmail.com>
2024-12-20 15:34:24 -0800

    Complete JS 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var tempString = string.split('');
+    var stringLen = string.length;
+    var n = stringLen - 1;
+    for(let i = 0; i <= n; i++)
+    {
+        let tempChar = tempString[n];
+        tempString[n] = tempString[i];
+        tempString[i] = tempChar;
+        n--;
+    }
+    return tempString.join("");
 
 };
 
