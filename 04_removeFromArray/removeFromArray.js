d21049725c5ffc53ac27b8840c5c5fccd8e5312e
David Truong <davidtruong063@gmail.com>
2024-12-20 16:09:10 -0800

    Complete JS 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    tempArray = [];
+    for(let char of arr)
+    {
+        var bool = true;
+        for(let i = 0; i < nums.length; i++)
+        {
+            if(char == nums[i] && typeof nums[i] == typeof char)
+            {
+                bool = false;
+                break;
+            }
+        }
+        if(bool) tempArray.push(char); 
+        bool = true;
+    }
+    return tempArray;
 };
 
 // Do not edit below this line
