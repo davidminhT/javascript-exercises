c4e4eeccd240a52194e354ccaa4762b44c6f7b19
David Truong <davidtruong063@gmail.com>
2024-12-20 16:53:44 -0800

    Complete JS 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  var result = ((fahrenheit-32) * 5/9);
+  if(result % 1 != 0)
+  {
+    return Number(result.toFixed(1));
+  }
+  return Number(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  var result = (celsius * 9/5 + 32)
+  if(result % 1 != 0)
+  {
+    return Number(result.toFixed(1));
+  }
+  return Number(result);
 };
 
 // Do not edit below this line
